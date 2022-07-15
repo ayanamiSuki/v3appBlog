def337a8cab240ca99a745adb83564fbda136849
wangyi <ayanamisuki@163.com>
2022-07-15 15:45:16 +0800

    fix:部分修正

---
+++ src/pages/login/Login.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container"></div>
+</template>
+
+<script lang="ts">
+export default {
+    name: 'LoginPage',
+    // 模板组件
+    components: {},
+    // 页面数据集合
+    data() {
+        return {}
+    },
+    // 加载完毕后触发
+    // mounted() {},
+    // 执行方法
+    methods: {},
+}
+</script>
